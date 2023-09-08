404b40d023a52170235b955a53e4aa30246005c8
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2023-09-08 12:58:01 -0400

    added htmlRoutes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const router = require('express').Router();
+
+// Landing page route
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Notes page route
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+// Add a catch-all route to redirect to the landing page if no matching route is found
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
